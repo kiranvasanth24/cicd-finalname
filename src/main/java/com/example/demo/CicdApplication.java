ae27c0a685fa4be906a028b0135aa65fd65453c5
kiranvasanth24 <kiranvasanth24@gmail.com>
2022-03-21 13:06:41 +0530

    first commit

--- src/main/java/com/example/demo/CicdApplication.java
+++ src/main/java/com/example/demo/CicdApplication.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +14,6 @@ import com.example.demo.Dao.CicdDao;
 import com.example.demo.Dto.CicdDto;
 
 @SpringBootApplication
-@RestController
 public class CicdApplication {
 	@Autowired
 	private CicdDao cicdDao;
@@ -22,11 +22,6 @@ public class CicdApplication {
 	 * @GetMapping public List<CicdDto> fetchOrders() { return cicdDao.getOrders();
 	 * }
 	 */
-	
-	@GetMapping("/orders")
-	public String show() {
-		return "yes";
-	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CicdApplication.class, args);
